6b6aedb6b4cccc4e7619a25abc14207e2860ed85
dbrown20 <89479931+dbrown20@users.noreply.github.com>
2022-10-18 20:34:44 -0400

    Add files via upload

---
+++ Pocket_Pantry/Pocket_Pantry/Pocket_PantryApp.swift
@@ -0,0 +1,17 @@
+//
+//  Pocket_PantryApp.swift
+//  Pocket_Pantry
+//
+//  Created by Devin Brown on 10/18/22.
+//
+
+import SwiftUI
+
+@main
+struct Pocket_PantryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
